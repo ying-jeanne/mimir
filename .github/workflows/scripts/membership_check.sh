309611444ed38459a461bc57ce1423d96b8ac69d
Ying WANG <74549700+ying-jeanne@users.noreply.github.com>
2023-10-26 21:19:03 +0200

    Update membership_check.sh

--- .github/workflows/scripts/membership_check.sh
+++ .github/workflows/scripts/membership_check.sh
@@ -10,20 +10,25 @@ if [ -z "$USERNAME" ] || [ -z "$TOKEN" ]; then
   exit 1
 fi
 
+# URL-encode the USERNAME
+ENCODED_USERNAME=$(printf %s "$USERNAME" | xxd -p -c 256 | tr -d '\n' | sed 's/\(..\)/%\1/g')
+
 # Set the GitHub API URL
-API_URL="https://api.github.com/orgs/grafana/teams/mimir-maintainers/members/$USERNAME"
+API_URL="https://api.github.com/orgs/grafana/teams/mimir-maintainers/members/$ENCODED_USERNAME"
 
 # Send a GET request to the GitHub API
 response=$(curl -s -o /dev/null -w "%{http_code}" -H "Authorization: token $TOKEN" $API_URL)
 
-echo "I don't understand this wtf"
-echo "$response"
-
-# Set the result as an environment variable
-if [ "$response" -eq 204 ]; then
-  echo "::set-output name=team_membership::true"
-elif [ "$response" -eq 404 ]; then
-  echo "::set-output name=team_membership::false"
-else
+# Check if the response is empty or non-integer
+if [ -z "$response" ]; then
   echo "::set-output name=team_membership::error"
+else
+  # Set the result as an environment variable based on the response code
+  if [ "$response" -eq 204 ]; then
+    echo "::set-output name=team_membership::true"
+  elif [ "$response" -eq 404 ]; then
+    echo "::set-output name=team_membership::false"
+  else
+    echo "::set-output name=team_membership::error"
+  fi
 fi
